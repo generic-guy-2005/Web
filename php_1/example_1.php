4bcb9e74a100dd45a5ef44b7e0f8789a38691748
generic-guy-2005 <jevirahmat0@gmail.com>
2025-04-24 11:49:37 +0700

    PHP Introduction

---
+++ php_1/example_1.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Variable</title>
+    </head>
+
+    <body>
+        <?php
+            $name = "Xi Jinping";
+            $year = 2012;
+
+            echo "Presiden China adalah = <b> $name <b>";
+            echo "<br>";
+            echo "Pada tahun: $year";
+            echo "<br> <br>";
+            echo "<b> Xi Jinping </b> adalah <i> pemegang kekuasaan tertinggi </i>";
+            echo "<h1> Xi Jinping punya banyak Social Credit (Taiwan) </h1>"   
+            
+            
+        ?>
+    </body>
+</html>
---
+++ php_1/module_1.php
@@ -0,0 +1,6 @@
+<?php
+$num1 = 10;
+$num2 = 10;
+$num3 = $num1 + $num2;
+print($num3);
+?>
